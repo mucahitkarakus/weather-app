6aecffac0de34085a7515b1fdcc4eecdbdd3b36f
mucahitkarakus <mocococo.80@gmail.com>
2022-10-01 16:08:14 +0300

    api done

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-const Input = () => {
+const Input = ({getLocation, getInfos}) => {
   return (
     <div className=" flex justify-center items-center">
       <div className="mt-7">
@@ -8,6 +8,12 @@ const Input = () => {
           className="px-3 py-2 sm:w-[25rem] bg-slate-300 text-black outline-none rounded-lg focus:bg-slate-200 placeholder:text-slate-500"
           type="text"
           placeholder="Enter a City"
+          onChange={(e) => getLocation(e.target.value)}
+          onKeyUp={(e) => {
+            if (e.key === "Enter") {
+              getInfos();
+            }
+          }}
         />
       </div>
     </div>
